d3833b18468a24d2281318bc98ceb79dfebdab95
Anna Mitchell <annaeileenmitchell@gmail.com>
2023-03-31 15:38:33 -0500

    added search bar

--- src/components/cats.js
+++ src/components/cats.js
@@ -4,11 +4,12 @@ const BASE_URL = `http://localhost:1337/api`;
 
 const Cats = (props) => {
   const [cats, setCats] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${BASE_URL}/cats`);
+        const response = await fetch(`${BASE_URL}/cats?q=${search}`);
         const result = await response.json();
         setCats(result);
       } catch (error) {
@@ -16,11 +17,16 @@ const Cats = (props) => {
       }
     };
     fetchData();
-  }, []);
+  }, [search]);
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
 
   return (
     <div>
       <h1 id="catHeader">Adoptable Cats</h1>
+      <input type="text" value={search} onChange={handleSearchChange} />
       {cats.length > 0 && cats.map(cat => (
         <div key={cat.id}>
           <div id="catProfile">
@@ -28,11 +34,6 @@ const Cats = (props) => {
             <img id="catImage" src={cat.imageURL}></img>
             <p>Breed: {cat.breed}</p>
             <p>Adoption Fee: {cat.adoptionFee}</p>
-
-            {/* Link to go to single cat */}
-            {/* <Link to={"/cats/" + cat.id} > View details </Link>
-            <SingleCat catProps={cat}/> */}
-            
           </div>
         </div>
       ))}
