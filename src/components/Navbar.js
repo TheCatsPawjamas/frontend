80c7555776ed7a74610405dc9cb2ede33f97043e
Talal-mahmoudi <tkm16c@my.fsu.edu>
2023-03-31 18:19:28 -0400

    worked on navbar a little

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import cart from "../images/Screenshot 2023-03-31 at 6.08.31 PM Small.jpeg";
 
 
 const Navbar = (props) => {
@@ -18,14 +19,17 @@ const Navbar = (props) => {
     return(
         <header>
             {/* <img id="headerLogo" src="/logo.jpg" alt="Fitness Tracker logos"/> */}
-                    <div id="headerButtonGroup">   
+                    <div id="headerButtonGroup">  
+
                         <Link to='/' className="headerButton"> Home </Link>
                         {/* <Link to='/routines' className="headerButton"> The Cats </Link>  */}
                         {/* {isLoggedIn ?<Link to='/myroutines' className="headerButton"> My Orders </Link>: undefined } */}
-                        {/* {!isLoggedIn ? undefined: <Link to='/cart'>{<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-T5lcPgKB-fLAURJz1axS817J2weWsEoeWA&usqp=CAU"></img>}</Link>} */}
+                        {/* {!isLoggedIn ? undefined: <Link to='/cart'><img src={cart}></img></Link>} */}
+                        
                         {/* {isLoggedIn ? <Link to='/myroutines' className="headerButton"> My Routines </Link> : <a href="#" onClick={() => alert("error")} className="headerButton"> My Routines </a>} */}
                         {/* <Link to='/activities' className="headerButton"> Activities </Link> */}
-                        {/* {!isLoggedIn ? <Link to='/login' className="headerButton"> Login </Link> : <Link onClick={handleLogout} className="headerButton"> Logout </Link>} */}
+                        {!isLoggedIn ? <Link to='/login' className="headerButton"> Login </Link> : <Link onClick={handleLogout} className="headerButton"> Logout </Link>}
+                        <Link to='/cart'><img src={cart}></img></Link>
                     </div>  
         </header>
     )
