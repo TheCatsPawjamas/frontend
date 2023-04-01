784f5debea6b68b9638a61f9b219b6e7047de5db
Talal-mahmoudi <tkm16c@my.fsu.edu>
2023-04-01 13:30:12 -0400

    worked on homepage, navbar, and main app components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Login } from "./Login"
 // export { default as Registration } from "./Registration"
 export {default as Navbar} from "./Navbar";
+export {default as Homepage} from "./Homepage";
+
