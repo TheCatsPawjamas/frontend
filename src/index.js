d347a5a52e5f73e06717754aac337ab2eb694071
Talal-mahmoudi <tkm16c@my.fsu.edu>
2023-04-01 13:39:33 -0400

    added the routes for allCats and singleCats and updated the export file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navbar, Homepage, Login } from "./components";
+import { Navbar, Homepage, Login, SingleProduct, Cats } from "./components";
 
 
 
@@ -15,6 +15,8 @@ const App = () => {
             <Routes>
                 <Route path='/' element={<Homepage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
                 <Route path='/login' element={<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
+                <Route path='/cats' element={<Cats isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
+                <Route path='/cats/:id' element={<SingleProduct isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
             </Routes> 
         </div>
     </BrowserRouter> 
