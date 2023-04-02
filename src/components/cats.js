806d220d549850ee59912e1008cacc1a7e34597f
Anna Mitchell <annaeileenmitchell@gmail.com>
2023-04-01 23:20:41 -0500

    made cats.css and added a link to SingleProducts

--- src/components/cats.js
+++ src/components/cats.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import "./cats.css"
 
 const BASE_URL = `http://localhost:1337/api`;
 
 const Cats = (props) => {
   const [cats, setCats] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${BASE_URL}/cats`);
+        const response = await fetch(`${BASE_URL}/cats?q=${search}`);
         const result = await response.json();
         setCats(result);
       } catch (error) {
@@ -16,11 +18,16 @@ const Cats = (props) => {
       }
     };
     fetchData();
-  }, []);
+  }, [search]);
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
 
   return (
     <div>
       <h1 id="catHeader">Adoptable Cats</h1>
+      <input type="text" value={search} onChange={handleSearchChange} />
       {cats.length > 0 && cats.map(cat => (
         <div key={cat.id}>
           <div id="catProfile">
@@ -28,6 +35,7 @@ const Cats = (props) => {
             <img id="catImage" src={cat.imageURL}></img>
             <p>Breed: {cat.breed}</p>
             <p>Adoption Fee: {cat.adoptionFee}</p>
+            <Link to={`/cats/${cat.id}`}>View {cat.name}'s details</Link>
           </div>
         </div>
       ))}
@@ -37,55 +45,3 @@ const Cats = (props) => {
 };
 
 export default Cats;
-
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-
-
-// //BASE_URL
-// const BASE_URL = `http://localhost:1337/api`;
-
-// const Cats = (props) => {
-//   const [cats, setCats] = useState([]);
-//   const [search, setSearch] = useState('');
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await fetch(`${BASE_URL}/cats?q=${search}`);
-//         const result = await response.json();
-//         setCats(result);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-//     fetchData();
-//   }, [search]);
-
-//   const handleSearchChange = (event) => {
-//     setSearch(event.target.value);
-//   };
-
-//   return (
-//     <div>
-//       <h1 id="catHeader">Adoptable Cats</h1>
-//       <input type="text" value={search} onChange={handleSearchChange} /> 
-//       {cats.length > 0 && cats.map(cat => (
-//         <div key={cat.id}>
-//           <div id="catProfile">
-//             <h2>{cat.name}</h2>
-//             <img id="catImage" src={cat.imageURL}></img>
-//             <p>Breed: {cat.breed}</p>
-//             <p>Adoption Fee: {cat.adoptionFee}</p>
-//           </div>
-//         </div>
-//       ))}
-//       {cats.length === 0 && <p>No cats found</p>}
-//     </div>
-//   );
-// };
-
-// export default Cats;
