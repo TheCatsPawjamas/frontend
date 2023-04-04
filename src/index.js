10729e72a30735d21410177289a4e7e17ae621ae
Anna Mitchell <annaeileenmitchell@gmail.com>
2023-04-04 01:50:40 -0500

    worked on Cart.js, Cart.css, SingleProducts.css

--- src/index.js
+++ src/index.js
@@ -3,25 +3,55 @@ import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar, Homepage, Login, SingleProduct, Cats, Registration, Cart } from "./components";
 
-
-
 const App = () => { 
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+
+
+    const addItemToCart = (item) => {
+        const index = cartItems.findIndex((cartItem) => cartItem.id === item.id);
+      
+        if (index === -1) {
+          setCartItems([...cartItems, { ...item, quantity: 1 }]);
+        } else {
+          const updatedCartItems = [...cartItems];
+          updatedCartItems[index].quantity++;
+          setCartItems(updatedCartItems);
+        }
+      };
+
+    const removeItemFromCart = (item) => {
+        const existingItemIndex = cartItems.findIndex((cartItem) => cartItem.id === item.id);
+        const existingItem = cartItems[existingItemIndex];
+      
+        let updatedItems = [];
+      
+        if (existingItem.quantity === 1) {
+          updatedItems = cartItems.filter((cartItem) => cartItem.id !== item.id);
+        } else {
+          const updatedItem = { ...existingItem, quantity: existingItem.quantity - 1 };
+          updatedItems = [...cartItems];
+          updatedItems[existingItemIndex] = updatedItem;
+        }
+      
+        setCartItems(updatedItems);
+      };
+      
 
     return ( 
-    <BrowserRouter>
-        <div> 
-            <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
-            <Routes>
-                <Route path='/' element={<Homepage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
-                <Route path='/login' element={<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
-                <Route path='/register' element={<Registration isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
-                <Route path='/cats' element={<Cats isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
-                <Route path='/cats/:id' element={<SingleProduct isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
-                <Route path='/cart' element={<Cart isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
-            </Routes> 
-        </div>
-    </BrowserRouter> 
+        <BrowserRouter>
+            <div> 
+                <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
+                <Routes>
+                    <Route path='/' element={<Homepage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+                    <Route path='/login' element={<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
+                    <Route path='/register' element={<Registration isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
+                    <Route path='/cats' element={<Cats addItemToCart={addItemToCart} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
+                    <Route path='/cats/:id' element={<SingleProduct isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}/>
+                    <Route path='/cart' element={<Cart cartItems={cartItems} addItemToCart={addItemToCart} removeItemFromCart={removeItemFromCart} />} />
+                </Routes> 
+            </div>
+        </BrowserRouter> 
     ) 
 }
 
