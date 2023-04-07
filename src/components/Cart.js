76ddf0242b9310b10e95c74cece14ce6d11e79f7
Talal-mahmoudi <tkm16c@my.fsu.edu>
2023-04-07 19:00:41 -0400

    worked on a couple bugs for the login handling and rendering pages

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,7 +4,7 @@ import './Cart.css';
 
 const Cart = (props) => {
 
-  const { cartItems, addItemToCart, removeItemFromCart } = props;
+  const { cartItems, addItemToCart, removeItemFromCart, isLoggedIn, setIsLoggedIn } = props;
 
 
   // const [items, setItems] = useState(
@@ -67,62 +67,71 @@ const Cart = (props) => {
 
   return (
     <div className="cart">
-      <h2>Your Cart</h2>
-      {cartItems.length === 0 && !cartItems ? (
-        <p id="emptyCart">Your cart is empty</p>
-      ) : (
-        <div>
-          <div className="cartItems">
-            {cartItems.map((item) => (
-              <div key={item.id} className="cartItem">
-                <div >
-                  <img className="cartItemImage" src={item.imageURL} alt={item.name} />
-                </div>
-                <div className="cartItemDetails">
-                  <p className="cartItemName">{item.name}</p>
-                  <p className="cartItemPrice">${Number(item.adoptionFee)}</p>
+      {!isLoggedIn? <h2>Please Sign in to add to a cart</h2>:
+      <div>
+        <h2>Your Cart</h2>
+        {cartItems?.length === 0 ? (
+          <p id="emptyCart">Your cart is empty</p>
+        ) : (
+          <div>
+            <div className="cartItems">
+              {cartItems?.map((item) => (
+                <div key={item.id} className="cartItem">
+                  <div >
+                    <img className="cartItemImage" src={item.imageURL} alt={item.name} />
+                  </div>
+                  <div className="cartItemDetails">
+                    <p className="cartItemName">{item.name}</p>
+                    <p className="cartItemPrice">${Number(item.adoptionFee)}</p>
 
-                  <div className="cartItemQuantity">
-                    <button
-                      onClick={() => {
-                        // const newQuantity = item.quantity > 1 ? item.quantity - 1 : 1;
-                        // handleQuantityChange(item, newQuantity);
-                        removeItemFromCart(item);
-                      }}
-                    >
-                      -
-                    </button>
-                    {/* <p>{item.quantity}</p>
-                    <button
-                      onClick={() => {
-                        // const newQuantity = item.quantity + 1;
-                        // handleQuantityChange(item, newQuantity);
-                        addItemToCart(item);
-                      }}
-                    >
-                      +
-                    </button> */}
+                    <div className="cartItemQuantity">
+                      <button
+                        onClick={() => {
+                          // const newQuantity = item.quantity > 1 ? item.quantity - 1 : 1;
+                          // handleQuantityChange(item, newQuantity);
+                          removeItemFromCart(item);
+                        }}
+                      >
+                        -
+                      </button>
+                      {/* <p>{item.quantity}</p>
+                      <button
+                        onClick={() => {
+                          // const newQuantity = item.quantity + 1;
+                          // handleQuantityChange(item, newQuantity);
+                          addItemToCart(item);
+                        }}
+                      >
+                        +
+                      </button> */}
+                    </div>
                   </div>
                 </div>
+              ))}
+            </div>
+            <div className="cartTotal">
+              <p>Total: ${totalPrice}</p>
+            </div>
+            <div className="bottomOfPage">
+              <div className="pjInfo">
+                <img id="pjImage" alt="Image of a cat wearing pajamas" src="https://img.ltwebstatic.com/images3_pi/2022/06/27/165631496683fc01fc37e2c05bb2ca0dacd1b1f4c1_thumbnail_900x.webp"/>
+                <button id="pjButton" onClick={handleAddPajamas}>Click here to add a cozy pair of pajamas for $25</button>
+                <p id="pjCount">Pajamas count: {pajamasCount}</p>
               </div>
-            ))}
-          </div>
-          <div className="cartTotal">
-            <p>Total: ${totalPrice}</p>
-          </div>
-          <div className="bottomOfPage">
-            <div className="pjInfo">
-              <img id="pjImage" alt="Image of a cat wearing pajamas" src="https://img.ltwebstatic.com/images3_pi/2022/06/27/165631496683fc01fc37e2c05bb2ca0dacd1b1f4c1_thumbnail_900x.webp"/>
-              <button id="pjButton" onClick={handleAddPajamas}>Click here to add a cozy pair of pajamas for $25</button>
-              <p id="pjCount">Pajamas count: {pajamasCount}</p>
-            </div>
-            <div>
-              <Link id="continueShoppingLink" to={`/cats`}>Continue shopping</Link>
-              <Link to={'/checkout'}> Checkout </Link>
+              <div>
+                <Link id="continueShoppingLink" to={`/cats`}>Continue shopping</Link>
+                <Link to={'/checkout'}> Checkout </Link>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+
+    </div>
+    }
+
+      
+      
+
     </div>
   );
 };
