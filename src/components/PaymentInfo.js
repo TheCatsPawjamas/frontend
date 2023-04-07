aa30e78ddd47ecddc037da3cdceaa5cdd33d0f4b
Aamna Syed <aamna.s.syed@gmail.com>
2023-04-07 13:10:13 -0500

    worked on paymentinfo css

--- src/components/PaymentInfo.js
+++ src/components/PaymentInfo.js
@@ -1,5 +1,6 @@
 import { useState, useEffect} from "react"
 import { Link } from "react-router-dom"
+import "./PaymentInfo.css"
 
 const PaymentInfo = (props) => {
     const { setIsLoggedIn, currentUser } = props
