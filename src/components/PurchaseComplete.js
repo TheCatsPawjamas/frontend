222d428f98c72ec8039c51d4850c5cd461644b8f
Aamna Syed <aamna.s.syed@gmail.com>
2023-04-09 12:39:53 -0500

    fixed typo

--- src/components/PurchaseComplete.js
+++ src/components/PurchaseComplete.js
@@ -2,13 +2,29 @@ import React from "react"
 import { useState } from "react"
 
 const PurchaseComplete = (props) => {
-    const { setIsLoggedIn, currentUser } = props
+    const { setIsLoggedIn, currentUser, cartItem, orderId } = props
     const [ confirmation, setConfirmation ] = useState("")
-    const [ orderId, setOrderId ] = useState("")
     const [ message, setMessage ] = useState("")
 
+    // do we need another fetch request that complete the order
+    async function CompleteOrder () {
+    try {
+        const response = await fetch(`http://localhost:1337/api/orders/purchaseComplete`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+        const result = await response.json();
+        console.log(result)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
     function sendMessage() {
-        setMessage("Your order is complete! Thank you for choosing Cat Pawjamas, please come and pickup your new furry friend from 1234 Purrrfect Lane, Meowville 051421. See you soon!");
+        setMessage("Your order is complete! Thank you for choosing Cat Pawjamas, please come and pickup your new furry friend from 1234 Purrrfect Lane Meowville, Milwaukitty 051421. See you soon!");
     }
 
     function createConfirmationNumber(){
@@ -20,18 +36,100 @@ const PurchaseComplete = (props) => {
         sendMessage();
         createConfirmationNumber()
     }
-
+// fetch request 2: 
     return (
-        <section id="purchaseCompleteSection"> 
+        <section id="purchaseCompleteSection">
             <div>
-                <h2> {currentUser.username}, click the button below for your order details </h2>
-                <button onClick={orderDetails}> Order Details </button>
-                <p> {message} </p>
-                <p> Confirmation Number: {confirmation} </p>
+                <h2> {currentUser.username}, Review Order Details </h2>
+                {
+                    cartItem.filter(item => item.creditCard === currentUser.creditCard).map((thing) => (
+                        <div key={thing}>
+                            <p>{thing.creditCard}</p>
+                            <p> Name On Credit Card: {thing.creditCardName}</p>
+                            <p> Credit Card Number: {thing.creditCard}</p>
+                        </div>
+                    ))
+                }
+
+                {/* {!cartItems.length && <div> Your Cart Is Empty </div>} */}
+                 
+                {
+                    cartItem.filter(item => item.creditCard === currentUser.creditCard).map((product) => {
+                        return (
+                            <section key={product.id}>
+                                <p> Cart: {product.name} </p>
+                                <p> </p>
+
+                                <div>
+                                {/* Display only the current user's credit card info */}
+                                    {product.creditCard === currentUser.creditCard && <p>{product.creditCard}</p>}
+                                </div>
+                            </section>
+                            ) 
+                        })
+                }
             </div>
         </section>
+
     )
 }
 
+    
 export default PurchaseComplete;
+
+{/* //     return (
+//         <section id="purchaseCompleteSection"> 
+//             <div>
+//                 <h2> {currentUser.username}, Review Order Details </h2> */}
+                // {
+                //     cartItems.filter(item => item.creditCard === currentUser.creditCard).map((products) => (
+                //         <div key={products.id}>
+                //             {products.name}
+                //         </div>
+                // }
+                //     )) => {
+                //         return (
+                //             <section>
+                //                 <div key={products.id}></div>
+                //             {/* How do i make it so it only shows the current users CC info  */}
+                //                 <div> </div>
+                //             </section>
+                        
+                //         )
+                //     }) : <div> Your Cart Is Empty </div>  
+                // }
+                
+//                 {/* orderId is not an array  */}
+//                 /* {
+//                     orderId ? orderId.map((products) => {
+//                         return (
+//                             <div key={products.id}></div>
+//                         )
+//                     }) : <div> Your Cart Is Empty </div>  
+//                 } */}
+                       
+        
+//                 <button onClick={CompleteOrder}> Order Details </button>
+//                 {/* <p> {message} </p> */}
+//                 <p> Confirmation Number: {confirmation} </p>
+//                 {/* <p> {cartItems}</p> */}
+//                 <p> orderId: {orderId}</p>
+//             </div>
+//         </section>
+//     )
+// }
+
+// export default PurchaseComplete;
+
+   {/* /* // .length ? null : <div>Your Cart Is Empty</div> */}
+                {/* //  */
+
+        //      
+        //         {/* <p> {message} </p> */}
+                // <p> Confirmation Number: {} </p>
+        //         {/* <p> {cartItems}</p> */}
+        //         <p> orderId: {}</p>
+        //     </div>
+        // </section> */} */}
+    }
 
