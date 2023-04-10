fe5b0ac53945dc4815c9911cde12d88a0a610091
Aamna Syed <aamna.s.syed@gmail.com>
2023-04-10 15:19:42 -0500

    worked on adminUsers and AdminUpdateUsers

---
+++ src/components/AdminUpdateUsers.js
@@ -0,0 +1,87 @@
+import {useState} from "react"
+// 
+const AdminUpdateUsers = (props) => {
+    const { isAdmin, setIsAdmin, user, users, setUsers } = props
+    const [ updateForm, setUpdateForm ] = useState (false)
+    const [ updatedUser, setUpdatedUser] = useState({})
+    const [ name, setName ] = useState("")
+    const [ password, setPassword ] = useState("")
+    const [ email, setEmail ] = useState ("") 
+    
+const updateUserForm = () => {
+    setUpdateForm(!updateForm)
+}
+
+
+    const updateUser = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch (`http://localhost:1337/api/users/admin/${user.id}`, {
+                method: "PATCH", 
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${localStorage.getItem("token")}`
+                },
+                body: JSON.stringify({
+                    username: name,
+                    email: email, 
+                    admin: false,
+                })
+            })
+
+            const updatedUserResponse = await response.json()
+
+            const updatedListOfUsers = users.filter ((singleUser) =>{
+                if (updatedUserResponse.id == singleUser.id) {
+                    return false
+                } else {
+                    return true
+                }
+            })
+
+            // setUsers(...updatedListOfUsers + updatedUserResponse)
+
+            setUsers([...updatedListOfUsers, updatedUserResponse])
+
+            console.log(updatedUserResponse)
+            setUpdatedUser(updatedUserResponse)
+        } catch (error) {
+            console.log( error)
+        }
+    }
+
+    return (
+        <section> 
+            <button onClick={updateUserForm}> Update User </button>
+
+            {updateForm ? (
+            <form id="updateUserForm" onSubmit={updateUser}>
+                
+                <h3> Update User</h3>
+                <label> 
+                    Username: 
+                    <input
+                    type="text"
+                    placeholder="New Username"
+                    value = {name}
+                    onChange = {(event) => setName(event.target.value)}
+                    />
+                </label>
+                <label>
+                    Email: 
+                    <input
+                        type="text"
+                        placeholder="New Email"
+                        value = {email}
+                        onChange = {(event) => setEmail(event.target.value)}
+                    />
+                </label>
+                <button type="submit" > Update User </button>
+
+            </form>
+        ): null }
+        </section>
+    )
+}
+
+export default AdminUpdateUsers
