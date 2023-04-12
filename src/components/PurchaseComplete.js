ee9c32c7252a90084161258068f7032764e2c7a9
Aamna Syed <aamna.s.syed@gmail.com>
2023-04-12 09:56:27 -0500

    pagination styling

--- src/components/PurchaseComplete.js
+++ src/components/PurchaseComplete.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { useState } from "react"
+import "./PurchaseComplete.css"
 
 const PurchaseComplete = (props) => {
     const { setIsLoggedIn, currentUser, cartItems, orderId, totalPrice, setTotalPrice } = props
@@ -10,7 +11,7 @@ const PurchaseComplete = (props) => {
         
         const confirmationNumber = Math.random().toString().substr(2,10);
         setConfirmation(confirmationNumber)
-        const message = ("Your order is complete! Thank you for choosing Cat Pawjamas, please come and pickup your new furry friend from 1234 Purrrfect Lane Meowville, Milwaukitty 051421. See you soon!")
+        const message = ("your order is complete! Thank you for choosing Cat Pawjamas, please come and pickup your new furry friend from 1234 Purrrfect Lane Meowville, Milwaukitty 051421. See you soon!")
         setMessage(message)
     }
 
@@ -37,31 +38,34 @@ const PurchaseComplete = (props) => {
     return (
         <section id="purchaseCompleteSection">
             <div>
-                <h2> {currentUser.username}, Review Order Details </h2>
+                <h2 id="purchaseCompleteHeader"> {currentUser.username}, Review Order Details </h2>
 
                 {
                     cartItems.filter(item => item.creditCard === currentUser.creditCard).map((product) => {
                         return (
-                            <section key={product.id}>
-                                <img src={product.imageURL} alt="Product Image"/>
-                                <p> UserID: {currentUser.id}</p>
-                                <p> Cart: {product.name} </p>
-                                <p> Cat ID: {product.id}</p>
-                                <p> Breed: {product.breed}</p>
-                                <p> Adoption Fee: {product.adoptionFee}</p>
-                                <p> Age: {product.age}</p>
-
-                                {/* only shows up when pajamas are added to the cart */}
-                                <p> Total After Tax: {Number(totalPrice) * .075 + Number(totalPrice)} </p> 
-                                <p> Order ID: {orderId}</p>
-                              
+                            <section id="orderDetailsSection"key={product.id}>
+                                <ul>
+                                    <img id="orderImage" src={product.imageURL} alt="Product Image"/>
+                                    <li>
+                                        <div id="orderDetails">
+                                            <p>UserID: {currentUser.id}</p>
+                                            <p>Cart: {product.name} </p>
+                                            <p>Cat ID: {product.id}</p>
+                                            <p>Breed: {product.breed}</p>
+                                            <p>Adoption Fee: {product.adoptionFee}</p>
+                                            <p>Age: {product.age}</p>
+                                            <p>Total After Tax: {Number(totalPrice) * .075 + Number(totalPrice)}</p> 
+                                            <p>Order ID: {orderId}</p>
+                                        </div>  
+                                    </li>
+                                </ul>
                             </section>
                             ) 
                         })
                 }
 
-            <button onClick={CompleteOrder}> Submit Order </button>
-            <p> {message}</p>
+            <button id="submitOrder" onClick={CompleteOrder}> Submit Order </button>
+            <p> {currentUser.username}, {message}</p>
             <p> Confirmation Number: {confirmation}</p>
             </div>
         </section>
